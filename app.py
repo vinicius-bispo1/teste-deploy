5c344a110cb0a41cd75cc37a21bc459660922a3b
vinicius-bispo1 <vini.bispo125@gmail.com>
2025-03-26 21:29:59 -0300

    código

--- app.py
+++ app.py
@@ -1,26 +1,21 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import sqlite3
-import validators
 from flask_cors import CORS
-
 
 app = Flask(__name__)
 CORS(app)
 
 
 def init_db():
-    try:
-        with sqlite3.connect('database.db') as conn:
-            conn.execute("""CREATE TABLE IF NOT EXISTS livros(
-                         id INTEGER PRIMARY KEY AUTOINCREMENT,
-                         titulo TEXT NOT NULL,
-                         categoria TEXT NOT NULL,
-                         autor TEXT NOT NULL,
-                         imagem_url TEXT NOT NULL
-                         )""")
-            print("Banco de dados criado com sucesso!")
-    except sqlite3.Error as e:
-        print(f"Erro ao criar banco de dados: {e}")
+    with sqlite3.connect('database.db') as conn:
+        conn.execute("""CREATE TABLE IF NOT EXISTS livros(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   titulo TEXT NOT NULL,
+                   categoria TEXT NOT NULL,
+                   autor TEXT NOT NULL,
+                   imagem_url TEXT NOT NULL
+                   )""")
+        print("Banco de dados criado!!")
 
 
 init_db()
@@ -28,38 +23,62 @@ init_db()
 
 @app.route('/')
 def home_page():
-    return "<h2>Minha página com Flask</h2>"
-
-
-def validar_link_http(url):
-    return bool(validators.url(url))
+    return render_template('index.html')
 
 
 @app.route('/doar', methods=['POST'])
 def doar():
-    try:
-        dados = request.get_json()
-        titulo = str(dados.get('titulo', ''))
-        categoria = str(dados.get('categoria', ''))
-        autor = str(dados.get('autor', ''))
-        imagem_url = str(dados.get('imagem_url', ''))
-        print(validar_link_http(imagem_url))
-        if not all([titulo, categoria, autor, imagem_url]):
-            return jsonify({"erro": "Todos os campos são obrigatórios"}), 400
 
-        if validar_link_http(imagem_url) != True:
-            return jsonify({"erro": "URL da imagem é inválida"}), 400
+    dados = request.get_json()
 
-        with sqlite3.connect('database.db') as conn:
-            conn.execute("""
-                INSERT INTO livros (titulo, categoria, autor, imagem_url)
-                VALUES (?, ?, ?, ?)
-            """, (titulo, categoria, autor, imagem_url))
-            conn.commit()
+    titulo = dados.get('titulo')
+    categoria = dados.get('categoria')
+    autor = dados.get('autor')
+    imagem_url = dados.get('imagem_url')
+
+    if not all([titulo, categoria, autor, imagem_url]):
+        return jsonify({'erro': 'Todos os campos são obrigatórios'}), 400
+
+    with sqlite3.connect('database.db') as conn:
+        conn.execute(""" INSERT INTO livros (titulo, categoria, autor, imagem_url)
+                     VALUES (?,?,?,?) 
+                     """, (titulo, categoria, autor, imagem_url))
+
+        conn.commit()
 
         return jsonify({"mensagem": "Livro cadastrado com sucesso"}), 201
-    except Exception as e:
-        return jsonify({"erro": f"Erro ao cadastrar o livro: {str(e)}"}), 500
+
+
+@app.route('/livros', methods=['GET'])
+def listar_livros():
+    with sqlite3.connect('database.db') as conn:
+        livros = conn.execute("SELECT * FROM livros").fetchall()
+
+    livros_formatados = []
+
+    for livro in livros:
+        dicionario_livros = {
+            "id": livro[0],
+            "titulo": livro[1],
+            "categoria": livro[2],
+            "autor": livro[3],
+            "imagem_url": livro[4]
+        }
+        livros_formatados.append(dicionario_livros)
+    return jsonify(livros_formatados)
+
+
+@app.route('/livros/<int:livro_id>', methods=['DELETE'])
+def deletar_livro(livro_id):
+    with sqlite3.connect('database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM livros WHERE id = ?", (livro_id,))
+        conn.commit()
+
+    if cursor.rowcount == 0:
+        return jsonify({"erro": "Livro não encontrado"}), 404
+
+    return jsonify({"menssagem": "Livro excluido com sucesso"}), 200
 
 
 if __name__ == '__main__':
